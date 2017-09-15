25608d3a6ddff11b10b9b1ef8390a8df92fb4f08
Jason <jason.mcgloin@gmail.com>
2017-09-14 22:18:05 -0400

    added search test

--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -9,4 +9,27 @@ describe('TodoSearch', () => {
 	it('should exist', () => {
 		expect(TodoSearch).toExist();
 	});
+
+	it('should call onSearch with search text', () => {
+		let spy = expect.createSpy();
+		let todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
+
+		let sText = 'Dog';
+
+		todoSearch.refs.searchText.value = sText;
+		TestUtils.Simulate.change(todoSearch.refs.searchText);
+
+		expect(spy).toHaveBeenCalledWith(false, 'Dog');
+	});
+
+	it('should call onSearch with checked value', () => {
+		let spy = expect.createSpy();
+		let todoSearch = TestUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
+
+		todoSearch.refs.showComplete.checked = true;
+		TestUtils.Simulate.change(todoSearch.refs.showComplete)
+
+		expect(spy).toHaveBeenCalledWith(true,'');
+
+	})
 });
