42fcb5d28d24bb78835cc72fd6f32634fc0d5d8b
Jason <jason.mcgloin@gmail.com>
2017-10-17 23:22:58 -0400

    add reducers and tests

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 export const searchTextReducer = (state = '', action) => {
 	switch(action.type) {
 		case 'SET_SEARCH_TEXT':
@@ -7,10 +9,24 @@ export const searchTextReducer = (state = '', action) => {
 	};
 };
 
-export const addTodoReducer = (state = [], action) => {
+export const todos = (state = [], action) => {
 	switch(action.type) {
 		case 'ADD_NEW_TODO':
 			return state.concat(action.newTodo);
+		case 'COMPLETE_TODO':
+			let todos = state.map( (todo) => {
+				if(todo.id === action.id) {
+					let newCompletedAt = todo.completed ? undefined : moment().unix();
+					let newCompleted = !todo.completed;
+
+					return {
+					...todo,
+					completed: newCompleted,
+					completedAt: newCompletedAt
+					};
+				}
+			});
+			return todos;
 		default:
 			return state;
 	};
@@ -20,15 +36,6 @@ export const toggleShowCompletedReducer = (state = false, action) => {
 	switch(action.type) {
 		case 'TOGGLE_SHOW_COMPLETED':
 			return !state;
-		default:
-			return state;
-	};
-};
-
-export const completeTodoReducer = (state = '', action) => {
-	switch(action.type) {
-		case 'COMPLETE_TODO':
-			return action.id;
 		default:
 			return state;
 	};
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,5 +1,7 @@
 const expect = require('expect');
 const df = require('deep-freeze-strict');
+const uuid = require('uuid');
+const moment = require('moment');
 
 const reducers = require('reducers');
 
@@ -20,4 +22,32 @@ describe('Reducers', () => {
 			expect(res).toEqual(true);
 		});
 	});
-});;
+	describe('todoReducer', () => {
+		let newTodo = {
+				id: uuid(),
+				text: 'Meditate',
+				completed: false,
+				createdAt: moment().unix(),
+				completedAt: undefined
+		};
+		it('should add a new todo', () => {
+			let action = {type: 'ADD_NEW_TODO', newTodo: newTodo};
+			let res = reducers.todos(df([]), df(action));
+
+			expect(res.length).toBe(1);
+			expect(res[0].text).toEqual(newTodo.text);
+		});
+		it('should toggle complete status', () => {
+			let action = {type: 'COMPLETE_TODO', id: newTodo.id};
+			let res = reducers.todos(df([newTodo]), df(action));
+
+			expect(res[0].completed).toEqual(true);
+			expect(res[0].completedAt).toNotEqual(undefined);
+
+			let newRes = reducers.todos(df(res), df(action));
+
+			expect(newRes[0].completed).toEqual(false);
+			expect(newRes[0].completedAt).toEqual(undefined);
+		});
+	});
+});
